7c2a539bbc4721115ca89eadea1714a965e4b7d8
Azee <aziztitu1996@gmail.com>
2018-07-05 17:15:46 -0500

    Adds Idle state for snake

---
+++ Assets/Scripts/Azee/AI/Snake/States/Idle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AI.SnakeStates
+{
+    public class Idle : StateMachine<Snake>.State
+    {
+        public class StateData
+        {
+
+        }
+
+        private static Idle _instance;
+
+        public static Idle Instance
+        {
+            get { return _instance ?? (_instance = new Idle()); }
+        }
+
+        private Idle()
+        {
+        }
+
+        public void Enter(Snake owner, params object[] args)
+        {
+
+        }
+
+        public void Update(Snake owner)
+        {
+
+        }
+
+        public void Exit(Snake owner)
+        {
+
+        }
+    }
+}
